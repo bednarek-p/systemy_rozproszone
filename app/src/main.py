e41731293ef362bbd1da1a71598c5e022976c963
piotr.bednarek <pbednarek@networkedassets.org>
2022-10-23 18:48:42 +0200

    Add API operations on postgres database

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,12 @@
-import psycopg2
+import json
+
 from fastapi import FastAPI
 
+from database import Database
+from todo import Todo
+
 app = FastAPI()
+postgres_db = Database("database_postgres", "prisma", "postgres", "postgres", "5432")
 
 
 @app.get("/")
@@ -9,33 +14,44 @@ async def read_root():
     return {"message": "Hello World"}
 
 
+@app.get("/api/get/todo/{id}")
+async def get_todo(id: int):
+    record = postgres_db.get_record_by_id(id)
+    return {"id": record[0], "name": record[1], "status": record[2]}
+
+
 @app.get("/api/get/todos/list")
-async def get_todo():
-    return {"todo": "This is test todo."}
+async def get_todos_list():
+    json_list = [{"id": record[0], "name": record[1], "status": record[2]} for record in postgres_db.get_all_records()]
+    sorted_json_list = sorted(json_list, key=lambda dict: dict["id"])
+    return sorted_json_list
 
 
-hostname = "database_postgres"
-database = "prisma"
-username = "postgres"
-password = "postgres"
-port_id = 5432
+@app.post("/api/create/todo")
+async def create_todo(todo: Todo):
+    postgres_db.insert_into_todos(todo.name, todo.status)
+    return {"name": todo.name, "status": todo.status}
 
 
-try:
-    connection = psycopg2.connect(host=hostname, dbname=database, user=username, password=password, port=port_id)
-    cursor = connection.cursor()
+@app.post("/api/update/todo/{id}")
+async def update_todo(id: int, todo: Todo):
+    postgres_db.update_record_by_id(id, todo.name, todo.status)
+    return {"name": todo.name, "status": todo.status}
 
-    create_script = """ CREATE TABLE IF NOT EXISTS todos (
-                                    id      int PRIMARY KEY,
-                                    name    varchar(100) NOT NULL,
-                                    status  varchar(40) NOT NULL)"""
-    cursor.execute(create_script)
 
-    connection.commit()
-except Exception as err:
-    print(err)
-finally:
-    if cursor is not None:
-        cursor.close()
-    if connection is not None:
-        connection.close()
+@app.post("/api/delete/todo/{id}")
+async def delete_todo(id: int):
+    deleted_record = postgres_db.get_record_by_id(id)
+    postgres_db.delete_record_by_id(id)
+    return deleted_record
+
+
+def main():
+    postgres_db.init_todos_table()
+    print("##########################################################")
+    postgres_db.insert_into_todos("Task nr 1", "To-do")
+    postgres_db.insert_into_todos("Task nr 2", "To-do")
+
+
+if __name__ == "__main__":
+    main()
